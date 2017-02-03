64950fe8e3a601d8823724ab2defd918d1106df0
Mauro Bringolf <mauro@webkinder.ch>
2017-02-03 18:09:56 +0100

    Applied Atom auto indentation

--- singleton.php
+++ singleton.php
@@ -1,14 +1,14 @@
 <?php
 /**
- * Plugin Name: Singleton Example
- * Plugin URI: https://github.com/maurobringolf/wordpress-plugin-singleton
- * Description: An empty illustration of a WordPress plugin implemented as singleton
- * Author: Mauro Bringolf
- * Author URI: https://maurobringolf.ch
- * Version: 1.0
- * Text Domain: singleton
- * Domain Path: languages
- */
+* Plugin Name: Singleton Example
+* Plugin URI: https://github.com/maurobringolf/wordpress-plugin-singleton
+* Description: An empty illustration of a WordPress plugin implemented as singleton
+* Author: Mauro Bringolf
+* Author URI: https://maurobringolf.ch
+* Version: 1.0
+* Text Domain: singleton
+* Domain Path: languages
+*/
 
 // Exit if accessed directly.
 if ( ! defined( 'ABSPATH' ) ) exit;
@@ -16,36 +16,36 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 if( ! class_exists( 'Singleton_Plugin_Illustration' ) ) {
 
   final class Singleton_Plugin_Illustration {
-
+    
     /**
-     * Stores the instance
-     */
+    * Stores the instance
+    */
     private static $instance;
 
     /**
-     * Stores the message to be displayed
-     */
+    * Stores the message to be displayed
+    */
     private $message;
 
     /**
-     * Makes sure that there is exactly one instance of this class stored inside the static attribute $instance.
-     * Acts as the constructor for this instance and returns the object.
-     */
+    * Makes sure that there is exactly one instance of this class stored inside the static attribute $instance.
+    * Acts as the constructor for this instance and returns the object.
+    */
     public static function instance() {
       if ( ! isset( self::$instance ) && ! ( self::$instance instanceof Singleton_Plugin_Illustration ) ) {
 
-  			self::$instance = new Singleton_Plugin_Illustration;
+        self::$instance = new Singleton_Plugin_Illustration;
         self::$instance->message = 'The singleton pattern works and prevents global scope pollution!';
 
-  			add_action( 'admin_notices', array( self::$instance, 'notify_user' ) );
+        add_action( 'admin_notices', array( self::$instance, 'notify_user' ) );
 
-  		}
-  		return self::$instance;
+      }
+      return self::$instance;
     }
 
     /**
-     * Outputs a WordPress notification ensuring that the singleton is working
-     */
+    * Outputs a WordPress notification ensuring that the singleton is working
+    */
     public function notify_user() {
 
       ?>
@@ -58,15 +58,15 @@ if( ! class_exists( 'Singleton_Plugin_Illustration' ) ) {
     }
 
     /**
-     * Disable cloning.
-     */
+    * Disable cloning.
+    */
     public function __clone() {
       //Throw error here.
     }
 
     /**
-     * Disable unserializing.
-     */
+    * Disable unserializing.
+    */
     public function __wakeup() {
       //Throw error here.
     }
@@ -76,8 +76,8 @@ if( ! class_exists( 'Singleton_Plugin_Illustration' ) ) {
 }
 
 /**
- * This function can be used by other plugins or themes to access any public API's this plugin offers.
- */
+* This function can be used by other plugins or themes to access any public API's this plugin offers.
+*/
 function SingletonPluginIllustration() {
   return Singleton_Plugin_Illustration::instance();
 }
